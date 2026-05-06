47c1187a1cac2ffbec5f00ce3598814e6d8e788b
speicon1911 <salvadorpeinado111906@gmail.com>
2026-05-06 10:39:43 +0200

    Update AlumnoController.java

--- src/main/java/ies/alcores/daweb/controller/AlumnoController.java
+++ src/main/java/ies/alcores/daweb/controller/AlumnoController.java
@@ -4,6 +4,7 @@ import ies.alcores.daweb.model.Alumno;
 import ies.alcores.daweb.service.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/alumno")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AlumnoController {
 
     @Autowired
     private AlumnoService alumnoService;
 
     @GetMapping
-    public ResponseEntity<List<Alumno>> all(){
+    public ResponseEntity<List<Alumno>> all() {
         return ResponseEntity.ok(this.alumnoService.findAll());
     }
 
