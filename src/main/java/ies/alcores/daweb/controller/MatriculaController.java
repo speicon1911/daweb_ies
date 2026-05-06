9427d2448c5e2faad3435dd28a02525a301abfdd
speicon1911 <salvadorpeinado111906@gmail.com>
2026-05-06 12:49:40 +0200

    se corrigen cosas

--- src/main/java/ies/alcores/daweb/controller/MatriculaController.java
+++ src/main/java/ies/alcores/daweb/controller/MatriculaController.java
@@ -4,6 +4,7 @@ import ies.alcores.daweb.model.Matricula;
 import ies.alcores.daweb.service.MatriculaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/matricula")
+@CrossOrigin("*")
 public class MatriculaController {
 
     @Autowired
